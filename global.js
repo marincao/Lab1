029d164ef2cd286da19430b856934fa997caeb7f
marincao <marincao626@gmail.com>
2025-04-25 21:04:41 -0700

    lab4

--- global.js
+++ global.js
@@ -71,3 +71,33 @@ if (localStorage.colorScheme) {
     select.value = localStorage.colorScheme;
     document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
 }
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  for (const project of projects) {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  }
+}
